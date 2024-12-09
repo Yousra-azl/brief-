a10e697fba4a472c782094437096111b5d44f770
Yousra-azl <yousraamzil@gmail.com>
2024-12-09 11:46:14 +0100

    fin du FizzBuzz

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,1 +1,14 @@
-{\rtf1}
+for (let nombre = 1; nombre <= 100; nombre++) {
+  if (nombre % 3===0 && nombre %5===0){
+    console.log("FizzBuzz");
+  }
+    else if (nombre % 3 ===0){
+      console.log("Fizz");
+    }
+  else if (nombre % 5 ===0){
+    console.log("Buzz");
+  }
+  else {
+    console.log(nombre);
+  }
+  }
